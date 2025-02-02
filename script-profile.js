b9a0412663744a74736ae07c2425d80e2fedc354
ItsJulius29 <julio_campos159@outlook.es>
2025-02-02 10:25:10 -0500

    nina-v13

--- script-profile.js
+++ script-profile.js
@@ -18,6 +18,36 @@ onAuthStateChanged(auth, (user) => {
     }
 });
 
+// Verificar si el usuario está autenticado nombre
+onAuthStateChanged(auth, (user) => {
+    if (user) {
+        // Si el usuario está autenticado, actualizar el navbar con su nombre
+        const navbarUsername = document.getElementById("nombre-usuario");
+        if (navbarUsername) {
+            navbarUsername.textContent = user.email.split("@")[0]; // Mostrar el nombre antes del @
+        }
+        localStorage.setItem("username",  user.email.split("@")[0]); // Guardar en localStorage
+    } else {
+        // Si NO hay usuario autenticado, redirigir al login
+        window.location.replace("login.html");
+    }
+});
+
+// Verificar si el usuario está autenticado correo
+onAuthStateChanged(auth, (user) => {
+    if (user) {
+        // Si el usuario está autenticado, actualizar el navbar con su nombre
+        const navbarUsername = document.getElementById("correo-usuario");
+        if (navbarUsername) {
+            navbarUsername.textContent = user.email; // Mostrar el nombre antes del @
+        }
+        localStorage.setItem("username", user.email); // Guardar en localStorage
+    } else {
+        // Si NO hay usuario autenticado, redirigir al login
+        window.location.replace("login.html");
+    }
+});
+
 // Función para cerrar sesión
 function cerrarSesion() {
     signOut(auth).then(() => {
