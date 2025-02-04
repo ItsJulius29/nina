7791b0ff721acfe9e301ad9f99953c5712a4b43e
ItsJulius29 <julio_campos159@outlook.es>
2025-02-03 22:10:50 -0500

    nina-v17

--- script-contacto.js
+++ script-contacto.js
@@ -43,82 +43,91 @@ var lugares = [
         coords: [-12.0599863, -77.1314227],
         title: "Cruce de miradas por primera vez",
         description: "El lugar donde comenzó nuestra primera aventura juntos.",
-        image: "img/img3.jpg"
+        image: "img/history/history1.jpg"
     },
     {
         coords: [  -12.0768298, -77.0824769241177],
         title: "Primer Beso🍽️",
         description: "Aquí tuvimos nuestra primer beso.",
-        image: "img/img1.webp"
+        image: "img/history/history16.jpg"
     },
     {
         coords: [-12.11667, -77.05],
         title: "Un Día Inolvidable 🌅",
         description: "Un hermoso atardecer que nunca olvidaremos.",
-        image: "img/img2.webp"
+        image: "img/history/history4.jpg"
     },
     {
         coords: [-12.129054, -77.035124],
         title: "Malecon - 21 de Sep",
         description: "Una hermosa tarde, salida por el 21 de septiembre y flores amarillas",
-        image: "img/img2.webp"
+        image: "img/nina/21sep.webp"
     },
     {
         coords: [-12.148867, -77.023070],
         title: "Barranco - 20 de Oct",
         description: "Un dia que nunca se podrá olvidar, porque aqui es donde oficializamos nuestro amor siendo enamoraditos.",
-        image: "img/img2.webp"
+        image: "img/history/history11.jpg"
     },
     {
         coords: [-12.060843, -77.036982],
         title: "Museo MALI",
         description: "Visita al museo como ninosh cultos.",
-        image: "img/img2.webp"
+        image: "img/galery/galery7.jpg"
     },
     {
         coords: [-12.136622, -77.023056],
         title: "Exposición del Color",
         description: "Visita a la esposicion del color, ese mismo dia fuimos enamorados.",
-        image: "img/img3.jpg"
+        image: "img/galery/galery12.jpg"
     },
     {
         coords: [-12.070622, -77.033280],
         title: "Parque de las Aguas",
         description: "Paseito por el circuito mágico del agua.",
-        image: "img/img1.webp"
+        image: "img/galery/galery14.jpg"
     },
     {
         coords: [-12.072640, -77.087048],
         title:"Parque de las leyendas",
         description: "Paseo al parque de las leyendas, ver monitos y dentro de un verano inolvidable",
-        image: "img/img2.webp"
+        image: "img/history/history22.jpg"
     },
     {
         coords: [-12.073460, -77.166048],
         title: "La punta ",
         description: "Respiro en la punta, conectando con el mar",
-        image: "img/img2.webp"
+        image: "img/nina/punta.jpg"
     },
     {
         coords: [-12.393834, -76.777289],
         title: "Playa San Bartolo",
         description: "Viaje a la playita con familia del monito",
-        image: "img/img2.webp"
+        image: "img/history/history31.jpg"
     },
     {
         coords: [-12.0876959, -77.0548991],
         title: "Peruano Japones Exposicion",
         description: "Visita a otra exposicion en un verano lindo",
-        image: "img/img2.webp"
+        image: "img/nina/perujapo.jpg"
     }
 ];
 
 // Objeto para almacenar los marcadores
 var markers = {};
 
-// Agregar los marcadores al mapa y almacenarlos
+// Crear un icono personalizado de color lila
+var customLilaIcon = L.divIcon({
+    className: 'custom-lila-icon', // Nombre de la clase
+    html: '<div class="lila-circle"></div>', // HTML para el icono
+    iconSize: [40, 40], // Tamaño del icono
+    iconAnchor: [20, 40], // El ancla de donde se posiciona el icono
+    popupAnchor: [0, -35], // Posición del popup
+});
+
+// Agregar los marcadores con el icono lila
 lugares.forEach((lugar, index) => {
-    var marker = L.marker(lugar.coords).addTo(map);
+    var marker = L.marker(lugar.coords, {icon: customLilaIcon}).addTo(map); // Aplicar el nuevo icono lila
 
     // Popup personalizado con HTML
     var popupContent = `
@@ -134,6 +143,7 @@ lugares.forEach((lugar, index) => {
     // Guardar el marcador en el objeto markers con el índice como clave
     markers[index] = marker;
 });
+
 
 // Seleccionar el contenedor de lugares
 var lugaresContainer = document.getElementById("lugares-container");
